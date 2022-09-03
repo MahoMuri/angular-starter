ad38b3e6fb56af50004130df197682a75c28ecf4
MahoMuri <paolo.santos_12@yahoo.com.ph>
2022-09-03 10:05:16 +0800

    feat: Added login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent {
+  username!: string;
+  password!: string;
+  errorMessage!: string;
+
+  constructor(private router: Router, private cookieService: CookieService) {}
+
+  logIn() {
+    if (this.username === 'mahomuri' && this.password === 'password123') {
+      this.cookieService.set('loggedAdminUser', this.username);
+      this.router.navigateByUrl('/dashboard');
+      console.log('Logged in!');
+    } else {
+      this.errorMessage = 'Incorrect username or password!';
+    }
+  }
+}
